addc10801b68634702f487ece266c2b8c4fa890e
Thomas Thorogood <thom562r@mail.hansenberg.dk>
2023-08-31 12:53:04 +0200

    yessir

--- MyNotes.cs
+++ MyNotes.cs
@@ -2,17 +2,7 @@ public static class MyNotes
 {
     public static string VariablesPageOne =
 @"...
-In the vast savannah of computer memory, a remarkable phenomenon occurs. 
-Behold, the magnificent 'variables' – they are the vessels of information, the heart of computation. 
-They bear names, bestowed upon them with care, starting only with letters or the humble underscore. 
-But tread with caution, for certain names are forbidden, like 'int', 'string', and 'bool'. 
-With the gentle touch of the '=' operator, values are imbued unto these variables. 
-And lo, their types can be declared, like incantations, as in 'type variableName = value;'. 
-Observe: 
- int age = 32;
- string name = ""Jesper"";
- float awesomeLevel = 99.9f;
- bool TooCoolForSchool = false;
+Variabler er seje fordi de kan bruges til mange forskellige ting.
 ";
 
     public static string VariablesPageTwo =
